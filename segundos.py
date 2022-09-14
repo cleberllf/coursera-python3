873145968e9dc2d555b93673aee55617f03c589f
Cleber Lima Filho <cleberllf@gmail.com>
2022-09-13 23:51:09 -0300

    Alteração no texto de saída do programa segundos.py

--- segundos.py
+++ segundos.py
@@ -6,4 +6,4 @@ horas = horas % 24
 segs_restantes = total_segs % 3600
 minutos = segs_restantes // 60
 segs_restantes_final = segs_restantes % 60
-print(dias, "dia(s),", horas, "hora(s),",minutos, "minuto(s) e",segs_restantes_final, "segundo(s).")
+print(dias, "dias,", horas, "horas,", minutos, "minutos e", segs_restantes_final, "segundos.")
