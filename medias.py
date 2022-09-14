2aa7cedb540524d09b8ca96ed25f706e84db93eb
Cleber Lima Filho <cleberllf@gmail.com>
2022-09-13 23:23:42 -0300

    Criado os programas medias.py (calcula a média aritmética de quatro notas) e quadrado.py (calcula o perímetro e a área de um quadrado)

---
+++ medias.py
@@ -0,0 +1,5 @@
+primeiraNota = float(input("Digite a primeira nota: "))
+segundaNota = float(input("Digite a segunda nota: "))
+terceiraNota = float(input("Digite a terceira nota: "))
+quartaNota = float(input("Digite a quarta nota: "))
+print("A média aritmética é", (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4)
